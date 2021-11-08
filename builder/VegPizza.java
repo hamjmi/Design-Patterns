98d633c046b6e56d6e162498e3c64a88c73a4bce
Mohd Hamid <31077875+hamjmi@users.noreply.github.com>
2021-11-08 18:49:43 +0530

    Add files via upload

---
+++ builder/VegPizza.java
@@ -0,0 +1,12 @@
+package com.hamid.designPatterns.builder;
+
+public class VegPizza extends Pizza{
+    public String name()
+    {
+        return "Veg Pizza";
+    }
+    public float price()
+    {
+        return 115.75f;
+    }
+}
