74a3ae5616066bf9fd56a8b05db471aa28d4adc7
Mohd Hamid <31077875+hamjmi@users.noreply.github.com>
2021-11-08 18:51:18 +0530

    Add files via upload

---
+++ builder/NonVegPizza.java
@@ -0,0 +1,12 @@
+package com.hamid.designPatterns.builder;
+
+public class NonVegPizza extends Pizza{
+    public String name()
+    {
+        return "Non-Veg Pizza";
+    }
+    public float price()
+    {
+        return 275.0f;
+    }
+}
